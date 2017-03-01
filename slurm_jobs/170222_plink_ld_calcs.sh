25a1305c45ed12858545a0b2919db6c8fc1b8c1b
djcotter <dcotter1@asu.edu>
2017-03-01 13:22:26 -0700

    edited the slurm script to convert VCF to all pops and calculate LD

--- slurm_jobs/170222_plink_ld_calcs.sh
+++ slurm_jobs/170222_plink_ld_calcs.sh
@@ -11,6 +11,26 @@
 
 date
 
-~/tools/plink2/bin/plink --bfile 
+cd ~/projects/1000_genomes/ld_par_boundary
+export PATH=$PATH:~/tools/bcftools/bin/
+export PATH=$PATH:~/tools/plink2/bin/
+
+MYTMPDIR=$(mktemp -d)
+function clean_up {
+	# Perform program exit housekeeping
+	rm -rf "$MYTMPDIR"
+}
+trap clean_up EXIT
+
+pops_file=population_list.txt
+readarray -t pops < $pops_file
+
+for i in ${pops[@]}
+do
+	bcftools view --samples-file ~/projects/1000_genomes/populations/subpops/${i}_females -Ov -o $MYTMPDIR/${i}_females_unfiltered_variants.vcf ~/projects/1000_genomes/ALL.chrX.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf
+	bcftools view -m2 -M2 -v snps -Ov -o $MYTMPDIR/${i}_females_biallelic_variants.vcf $MYTMPDIR/${i}_females_unfiltered_variants.vcf
+	plink --vcf $MYTMPDIR/${i}_females_biallelic_variants.vcf --out plink_files/${i}_females_biallelic_variants.vcf
+	#plink --bfile plink_files/${i}_females_biallelic_variants.vcf --out 
+done
 
 date 
