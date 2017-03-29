640c9dc9c1b1e475e427741aa512e0d6d4a42c3d
djcotter <dcotter1@asu.edu>
2017-03-29 13:53:30 -0700

    Cythonized average_ld_By_window.py script

---
+++ scripts/ld_scripts/average_ld_by_window/average_ld_by_window.py
@@ -0,0 +1,43 @@
+"""The purpose of this script is to take pairwise LD output from plink and calculate average LD in set windows
+	Updated: 3/30/17
+	Daniel Cotter
+"""
+
+# import required packages
+from sys import argv
+import csv
+import numpy as np
+import time
+import ld_analysis
+
+############################
+## Set argument variables ##
+############################
+""" input file: pairwise LD file generated in space-delimited table format 
+				by plink using the command 'with-freqs' (essential so the columns are correctly indexed)
+	windows: bed file with desired windows for the analysis
+	ld_bin_size: length in either direction from the focal position in which we should consider LD (in kilobases)
+	analysis type: type of summary file to be generated using the desired options
+	output_file: path to an output file
+"""
+script, input_LD, windows_input, ld_bin_length = argv
+ld_bin_length = int(ld_bin_length)
+#script, input_file, windows, ld_bin_size, analysis_type, output_file = argv
+
+######################################################
+## Read in windows file and set starting conditions ##
+######################################################
+# Open the window file as bed coordinates so that every window_coordinates[i][1] corresponds to the start position and [2] to the end position.
+with open(windows_input, 'rU') as f:
+	window_file = list(csv.reader(f, delimiter = '\t'))
+	for window in window_file:
+			window[1] = float(window[1])
+			window[2] = float(window[2]) 
+			del window[3]
+
+############################################################
+## Open the LD file and perform the analysis line by line ##
+############################################################
+ld_calculations = ld_analysis.LD_loop(input_LD, window_file, ld_bin_length)
+
+print ld_calculations
