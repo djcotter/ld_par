3a73daaef15b6338f66658b80606ce71426d7be6
djcotter <dcotter1@asu.edu>
2017-04-20 12:00:20 -0700

    edited avg_ld.r

--- graphs/avg_ld.R
+++ graphs/avg_ld.R
@@ -1,7 +1,10 @@
+YRI_females_avg_R2_100kb_windows_1Mb_ldBins <- read_delim("~/Desktop/YRI_females_avg_R2_100kb_windows_1Mb_ldBins.txt", 
+                                                          +     "\t", escape_double = FALSE, col_names = FALSE, 
+                                                          +     trim_ws = TRUE)
 ld_data <- YRI_females_avg_R2_100kb_windows_1Mb_ldBins
 ld_data <- YRI_females_avg_R2_100kb_windows_200kb_ldBins
 
 ld_data$position <- ((ld_data$X1 + ld_data$X2)/2)/1000000
-ld_data_subset <- subset(ld_data, ld_data$position <= 30)
+ld_data_subset <- subset(ld_data, ld_data$position <= 7)
 plot(ld_data$position, ld_data$X3, pch=20, ylim=c(0,1))
 plot(ld_data_subset$position, ld_data_subset$X3, pch=20)
